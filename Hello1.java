70dbcdf8635dfcf414bd305e3c0ac6a7e3255114
yz <610467586@qq.com>
2019-05-17 18:00:00 +0800

    second

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,5 @@
 ﻿public class Hello{
 	public static void main(String [] args){
-	System.out.println("new Hello13");
+	System.out.println("new Hello1");
 	}
 }
--- Hello2.java
+++ Hello2.java
@@ -1,5 +1,5 @@
 ﻿public class Hello{
 	public static void main(String [] args){
-	System.out.println("Hello2");
+	System.out.println("Hello21");
 	}
 }
